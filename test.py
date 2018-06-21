4bcc558cba66c1cc6a8e616c94ef0bd3340f7615
ZenTuRy <39700519+ZenTuRyz@users.noreply.github.com>
2018-06-21 19:57:46 +0700

    Update test.py

--- test.py
+++ test.py
@@ -78,7 +78,7 @@ def helpmessage():
     helpMessage = " ⚠คำสั่งช่วยเหลือ⚠" + "\n" + \
                   "⭐help ➠ จะแสดงคำสั่ง" + "\n" + \
                   "⭐help2 ➠ จะแสดงคำสั่งชุดที่ 2" + "\n" + \
-                  "⭐help3 ➠ จะแสดงคำสั่งชุดที่ 3" + "\n" + \
+                  "⭐!help ➠ จะแสดงคำสั่ง OHM" + "\n" + \
                   "⭐รีบอท ➠ บอทจะเริมทำงานใหม่" + "\n" + \
 		  " ⚠คำสั่งสถานะ⚠" + "\n" + \
                   "⭐sp ➠ จะแสดงความเร็วบอท" + "\n" + \
@@ -158,118 +158,6 @@ def helpohm():
                          "⭐!tagall ➠ เช็คว่าบอทหลุดไหม" + "\n" + \
                   "Created by : ꧁OHM꧂ "
     return helpOhm
-
-def helptranslate():
-    helpTranslate =    "╔══[ T R A N S L A T E ]" + "\n" + \
-                       "╠ af : afrikaans" + "\n" + \
-                       "╠ sq : albanian" + "\n" + \
-                       "╠ am : amharic" + "\n" + \
-                       "╠ ar : arabic" + "\n" + \
-                       "╠ hy : armenian" + "\n" + \
-                       "╠ az : azerbaijani" + "\n" + \
-                       "╠ eu : basque" + "\n" + \
-                       "╠ be : belarusian" + "\n" + \
-                       "╠ bn : bengali" + "\n" + \
-                       "╠ bs : bosnian" + "\n" + \
-                       "╠ bg : bulgarian" + "\n" + \
-                       "╠ ca : catalan" + "\n" + \
-                       "╠ ceb : cebuano" + "\n" + \
-                       "╠ ny : chichewa" + "\n" + \
-                       "╠ zh-cn : chinese (simplified)" + "\n" + \
-                       "╠ zh-tw : chinese (traditional)" + "\n" + \
-                       "╠ co : corsican" + "\n" + \
-                       "╠ hr : croatian" + "\n" + \
-                       "╠ cs : czech" + "\n" + \
-                       "╠ da : danish" + "\n" + \
-                       "╠ nl : dutch" + "\n" + \
-                       "╠ en : english" + "\n" + \
-                       "╠ eo : esperanto" + "\n" + \
-                       "╠ et : estonian" + "\n" + \
-                       "╠ tl : filipino" + "\n" + \
-                       "╠ fi : finnish" + "\n" + \
-                       "╠ fr : french" + "\n" + \
-                       "╠ fy : frisian" + "\n" + \
-                       "╠ gl : galician" + "\n" + \
-                       "╠ ka : georgian" + "\n" + \
-                       "╠ de : german" + "\n" + \
-                       "╠ el : greek" + "\n" + \
-                       "╠ gu : gujarati" + "\n" + \
-                       "╠ ht : haitian creole" + "\n" + \
-                       "╠ ha : hausa" + "\n" + \
-                       "╠ haw : hawaiian" + "\n" + \
-                       "╠ iw : hebrew" + "\n" + \
-                       "╠ hi : hindi" + "\n" + \
-                       "╠ hmn : hmong" + "\n" + \
-                       "╠ hu : hungarian" + "\n" + \
-                       "╠ is : icelandic" + "\n" + \
-                       "╠ ig : igbo" + "\n" + \
-                       "╠ id : indonesian" + "\n" + \
-                       "╠ ga : irish" + "\n" + \
-                       "╠ it : italian" + "\n" + \
-                       "╠ ja : japanese" + "\n" + \
-                       "╠ jw : javanese" + "\n" + \
-                       "╠ kn : kannada" + "\n" + \
-                       "╠ kk : kazakh" + "\n" + \
-                       "╠ km : khmer" + "\n" + \
-                       "╠ ko : korean" + "\n" + \
-                       "╠ ku : kurdish (kurmanji)" + "\n" + \
-                       "╠ ky : kyrgyz" + "\n" + \
-                       "╠ lo : lao" + "\n" + \
-                       "╠ la : latin" + "\n" + \
-                       "╠ lv : latvian" + "\n" + \
-                       "╠ lt : lithuanian" + "\n" + \
-                       "╠ lb : luxembourgish" + "\n" + \
-                       "╠ mk : macedonian" + "\n" + \
-                       "╠ mg : malagasy" + "\n" + \
-                       "╠ ms : malay" + "\n" + \
-                       "╠ ml : malayalam" + "\n" + \
-                       "╠ mt : maltese" + "\n" + \
-                       "╠ mi : maori" + "\n" + \
-                       "╠ mr : marathi" + "\n" + \
-                       "╠ mn : mongolian" + "\n" + \
-                       "╠ my : myanmar (burmese)" + "\n" + \
-                       "╠ ne : nepali" + "\n" + \
-                       "╠ no : norwegian" + "\n" + \
-                       "╠ ps : pashto" + "\n" + \
-                       "╠ fa : persian" + "\n" + \
-                       "╠ pl : polish" + "\n" + \
-                       "╠ pt : portuguese" + "\n" + \
-                       "╠ pa : punjabi" + "\n" + \
-                       "╠ ro : romanian" + "\n" + \
-                       "╠ ru : russian" + "\n" + \
-                       "╠ sm : samoan" + "\n" + \
-                       "╠ gd : scots gaelic" + "\n" + \
-                       "╠ sr : serbian" + "\n" + \
-                       "╠ st : sesotho" + "\n" + \
-                       "╠ sn : shona" + "\n" + \
-                       "╠ sd : sindhi" + "\n" + \
-                       "╠ si : sinhala" + "\n" + \
-                       "╠ sk : slovak" + "\n" + \
-                       "╠ sl : slovenian" + "\n" + \
-                       "╠ so : somali" + "\n" + \
-                       "╠ es : spanish" + "\n" + \
-                       "╠ su : sundanese" + "\n" + \
-                       "╠ sw : swahili" + "\n" + \
-                       "╠ sv : swedish" + "\n" + \
-                       "╠ tg : tajik" + "\n" + \
-                       "╠ ta : tamil" + "\n" + \
-                       "╠ te : telugu" + "\n" + \
-                       "╠ th : thai" + "\n" + \
-                       "╠ tr : turkish" + "\n" + \
-                       "╠ uk : ukrainian" + "\n" + \
-                       "╠ ur : urdu" + "\n" + \
-                       "╠ uz : uzbek" + "\n" + \
-                       "╠ vi : vietnamese" + "\n" + \
-                       "╠ cy : welsh" + "\n" + \
-                       "╠ xh : xhosa" + "\n" + \
-                       "╠ yi : yiddish" + "\n" + \
-                       "╠ yo : yoruba" + "\n" + \
-                       "╠ zu : zulu" + "\n" + \
-                       "╠ fil : Filipino" + "\n" + \
-                       "╠ he : Hebrew" + "\n" + \
-                       "╚══[ Jangan Typo ]" + "\n" + "\n\n" + \
-                         "Contoh : tr-id Nadya Cantik"
-    return helpTranslate
 #==============================================================================#
 def lineBot(op):
     try:
@@ -318,9 +206,6 @@ def lineBot(op):
                 elif text.lower() == 'help3':
                     helpOhm = helpohm()
                     nadya.sendMessage(to, str(helpOhm))
-                elif text.lower() == 'translate':
-                    helpTranslate = helptranslate()
-                    nadya.sendMessage(to, str(helpTranslate))
 #==============================================================================#
                 elif "ผส" == msg.text.lower():
                     nadya.sendMessage(to,"ผู้สร้างบอท\n ꧁OHM꧂  ")
