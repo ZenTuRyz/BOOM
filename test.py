9857da4a661a4029edeba4dbd41b6a140f987e9f
ZenTuRy <39700519+ZenTuRyz@users.noreply.github.com>
2018-06-24 19:58:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -46,8 +46,7 @@ def main():
     number_of_times = int(input("Choose the number of times you want to spam. "))
     message = input("What message do you want to spam? ")
     for _ in range(number_of_times):
-        print(message)
-		print((message + '\n') * number_of_times )
+        print((message + '\n')
 
 def restartBot():
     print ("[ INFO ] BOT RESETTED")
