dae1d0f5f108ce71985e3ef3929ef66ba7ff6c14
ZenTuRy <39700519+ZenTuRyz@users.noreply.github.com>
2018-06-24 20:26:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1288,12 +1288,7 @@ def lineBot(op):
                     else:
                         pass
 
-#==============================================================================#
-def main():
-    number_of_times = int(input("Choose the number of times you want to spam. "))
-    message = input("What message do you want to spam? ")
-    for _ in range(number_of_times):
-        print((message + '\n')
+
 #==============================================================================#
         if op.type == 26:
             print ("[ 26 ] RECEIVE MESSAGE")
